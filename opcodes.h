796d753fc285afaa1843edbc3cf5fee493eb762d
Jaskaran Singh <jaskaran.singh58@nic.in>
2026-01-04 23:04:20 +0530

    initial commit

---
+++ opcodes.h
@@ -0,0 +1,28 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+// Data Movement
+#define PUSH 0x01
+#define POP  0x02
+#define DUP  0x03
+#define HALT 0xFF
+
+// Arithmetic
+#define ADD  0x10
+#define SUB  0x11
+#define MUL  0x12
+#define DIV  0x13
+#define CMP  0x14
+
+// Control Flow
+#define JMP  0x20
+#define JZ   0x21
+#define JNZ  0x22
+
+// Memory & Functions
+#define STORE 0x30
+#define LOAD  0x31
+#define CALL  0x40
+#define RET   0x41
+
+#endif
